d1b55a472b32532938ace5509c87bd41454b047e
shabinder <dev.shabinder@gmail.com>
2021-04-15 21:46:03 +0530

    CInterop with Curl and MacOS and Windows Targets Added

--- build.gradle.kts
+++ build.gradle.kts
@@ -56,15 +56,13 @@ kotlin {
         }
     }
     ios()
-    /*
-    TODO:
-     Native Targets (libCurl Interop issue)
-    macosX64()
-    mingwX64()
-    linuxX64() *//*{
-        compilations.main.cinterops {
-            interop
-        }
+    /*TODO:
+     Native Targets (libCurl Interop issue)*/
+    macosX64() { compilations.getByName("main").cinterops.create("curl")  }
+    mingwX64() { compilations.getByName("main").cinterops.create("curl")  }
+    //linuxX64() { compilations.getByName("main").cinterops.create("curl") }
+    /*targets.withType<org.jetbrains.kotlin.gradle.plugin.mpp.KotlinNativeTarget>().forEach { target ->
+        target.compilations.getByName("main").cinterops.create("curl")
     }*/
     sourceSets {
         val commonMain by getting {
@@ -110,7 +108,7 @@ kotlin {
             }
         }
         val iosTest by getting
-        /*val desktopCommonMain by creating {
+        val desktopCommonMain by creating {
             dependsOn(commonMain)
             dependencies {
                 implementation("io.ktor:ktor-client-curl:$ktorVersion")
@@ -121,17 +119,16 @@ kotlin {
         }
         val mingwX64Main by getting
         val macosX64Main by getting
-        val linuxX64Main by getting
-        configure(listOf(mingwX64Main, macosX64Main, linuxX64Main)) {
+//        val linuxX64Main by getting
+        configure(listOf(mingwX64Main, macosX64Main/*, linuxX64Main*/)) {
             dependsOn(desktopCommonMain)
         }
-
         val mingwX64Test by getting
         val macosX64Test by getting
-        val linuxX64Test by getting
-        configure(listOf(mingwX64Test, macosX64Test, linuxX64Test)) {
+//        val linuxX64Test by getting
+        configure(listOf(mingwX64Test, macosX64Test/*, linuxX64Test*/)) {
             dependsOn(desktopCommonTest)
-        }*/
+        }
         /*val hostOs = System.getProperty("os.name")
         val isMingwX64 = hostOs.startsWith("Windows")
         val nativeTarget = when {
